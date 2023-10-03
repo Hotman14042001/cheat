09dc1a588c1de6e4041649c25706ef9b621d4231
Hotman1404 <111185602+Hotman14042001@users.noreply.github.com>
2023-10-03 19:03:21 +0700

    Update and rename cek_lokasi.py to lokasi.py

--- lokasi.py
+++ lokasi.py
@@ -1,5 +1,4 @@
 import geocoder
-import pygame
 
 # Alamat yang ingin Anda cek lokasinya
 alamat = "Jalan Contoh No. 123, Kota Contoh, Indonesia"
@@ -11,16 +10,5 @@ if lokasi.ok:
     print(f"Lokasi: {lokasi.address}")
     print(f"Latitude: {lokasi.latlng[0]}")
     print(f"Longitude: {lokasi.latlng[1]}")
-
-    # Inisialisasi pygame
-    pygame.mixer.init()
-
-    # Nama file musik atau suara yang ingin Anda putar
-    file_musik = "manullang11404.mp3"  # Ganti "nama_file_musik.mp3" dengan nama file Anda
-
-    # Memutar musik
-    pygame.mixer.music.load(file_musik)
-    pygame.mixer.music.play()
-
 else:
     print("Tidak dapat menemukan lokasi.")
